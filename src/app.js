cca192fe7d68f4f0b1139ae6dbb0c67911b43357
Dean Johnson <johnson.kennethdean@gmail.com>
2018-02-06 11:18:54 -0800

    Add redux components set up state maping

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense, removeExpense, editExpense } from './actions/expenses';
@@ -14,8 +15,18 @@ store.dispatch(addExpense({ description: 'Water bill' }));
 store.dispatch(addExpense({ description: 'Gas bill' }));
 store.dispatch(setTextFilter('water'));
 
+setTimeout(() => {
+	store.dispatch(setTextFilter('rent'));
+}, 3000);
+
 const state = store.getState();
 const visableExpenses = getVisableExpenses(state.expenses, state.filters);
 console.log(visableExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+	<Provider store={store}>
+		<AppRouter />
+	</Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
